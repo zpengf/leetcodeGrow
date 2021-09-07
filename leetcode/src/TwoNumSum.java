23932de82a25aa10b88368455e38c121e8a1873a
zhangPengfei <pengfei.zhang03@hand-china.com>
2021-09-07 20:27:05 +0800

    [add]两数之和

--- leetcode/src/TwoNumSum.java
+++ leetcode/src/TwoNumSum.java
@@ -48,6 +48,22 @@ public class TwoNumSum {
                 }
             }
         }
+
+
+        // 建立k-v ，一一对应的哈希表
+//        HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
+//        for(int i = 0; i < nums.length; i++){
+//            if(hash.containsKey(nums[i])){
+//                indexs[0] = i;
+//                indexs[1] = hash.get(nums[i]);
+//                return indexs;
+//            }
+//            // 将数据存入 key为补数 ，value为下标
+//            hash.put(target-nums[i],i);
+//        }
+
+
+
         return result;
     }
 
