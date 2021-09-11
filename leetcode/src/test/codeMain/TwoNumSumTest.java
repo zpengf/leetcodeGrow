43cd9a6228d5d48501f31bd1eebe60dcf7b45200
zhangPengfei <pengfei.zhang03@hand-china.com>
2021-09-11 13:01:00 +0800

    [add]添加测试单元

---
+++ leetcode/src/test/codeMain/TwoNumSumTest.java
@@ -0,0 +1,45 @@
+package codeMain;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/** 
+* TwoNumSum Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>09/11/2021</pre> 
+* @version 1.0 
+*/ 
+public class TwoNumSumTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: main(String args[]) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: twoSum(int[] nums, int target) 
+* 
+*/ 
+@Test
+public void testTwoSum() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
